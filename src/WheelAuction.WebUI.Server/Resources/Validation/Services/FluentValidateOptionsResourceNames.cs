44780437bec446f35c4d3178bf5c172b7795bba6
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:12:39 +0300

    feat(validation): add a service to validate options using Fluent Validation

---
+++ src/WheelAuction.WebUI.Server/Resources/Validation/Services/FluentValidateOptionsResourceNames.cs
@@ -0,0 +1,6 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Validation.Services;
+
+internal static class FluentValidateOptionsResourceNames
+{
+	public const string ErrorMessageValidationFailure = "ErrorMessage_ValidationFailure";
+}
---
+++ src/WheelAuction.WebUI.Server/Validation/Services/FluentValidateOptions.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using WheelAuction.WebUI.Server.Localization.Extensions;
+using WheelAuction.WebUI.Server.Resources.Validation.Services;
+
+namespace WheelAuction.WebUI.Server.Validation.Services;
+
+internal class FluentValidateOptions<TOptions> : IValidateOptions<TOptions> where TOptions : class
+{
+	private readonly IServiceScopeFactory _serviceScopeFactory;
+	private readonly IStringLocalizer _stringLocalizer;
+	private readonly string? _name;
+
+	public FluentValidateOptions(
+		IServiceScopeFactory serviceScopeFactory,
+		IStringLocalizerFactory stringLocalizerFactory,
+		string? name)
+	{
+		(_serviceScopeFactory, _stringLocalizer, _name) = (
+			serviceScopeFactory,
+			stringLocalizerFactory.CreateForGenericService(typeof(FluentValidateOptions<>))!,
+			name);
+	}
+
+	public ValidateOptionsResult Validate(string? name, TOptions options)
+	{
+		if ((_name is not null) && (_name != name))
+			return ValidateOptionsResult.Skip;
+
+		using var scope = _serviceScopeFactory.CreateScope();
+		IValidator<TOptions> validator = scope.ServiceProvider.GetRequiredService<IValidator<TOptions>>();
+
+		ValidationResult validationResult = validator.Validate(options);
+
+		if (validationResult.IsValid)
+			return ValidateOptionsResult.Success;
+
+		string optionsTypeName = typeof(TOptions).Name;
+		List<string> validationFailureErrorMessages = [];
+
+		foreach (ValidationFailure validationFailure in validationResult.Errors)
+		{
+			validationFailureErrorMessages.Add(_stringLocalizer[
+				FluentValidateOptionsResourceNames.ErrorMessageValidationFailure,
+				optionsTypeName,
+				validationFailure.PropertyName,
+				validationFailure.ErrorMessage]);
+		}
+
+		return ValidateOptionsResult.Fail(validationFailureErrorMessages);
+	}
+}
