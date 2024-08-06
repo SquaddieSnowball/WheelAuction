f17469031c9e2920b404f731f95e5a14cbec69c3
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:13:12 +0300

    feat(configuration): add a type containing extensions to the options builder

---
+++ src/WheelAuction.WebUI.Server/Configuration/Options/Extensions/OptionsBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using WheelAuction.WebUI.Server.Validation.Services;
+
+namespace WheelAuction.WebUI.Server.Configuration.Options.Extensions;
+
+internal static class OptionsBuilderExtensions
+{
+	public static OptionsBuilder<TOptions> ValidateFluentValidation<TOptions>(this OptionsBuilder<TOptions> optionsBuilder)
+		where TOptions : class
+	{
+		optionsBuilder.Services
+			.AddSingleton<IValidateOptions<TOptions>>(serviceProvider =>
+				new FluentValidateOptions<TOptions>(
+					serviceProvider.GetRequiredService<IServiceScopeFactory>(),
+					serviceProvider.GetRequiredService<IStringLocalizerFactory>(),
+					optionsBuilder.Name));
+
+		return optionsBuilder;
+	}
+}
