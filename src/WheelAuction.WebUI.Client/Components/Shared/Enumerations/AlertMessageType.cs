ce5e560a697286696e6caf5ed9431109eaf611be
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 18:20:01 +0300

    feat(component): add a component used to display the alert message

---
+++ src/WheelAuction.WebUI.Client/Components/Shared/Enumerations/AlertMessageType.cs
@@ -0,0 +1,11 @@
+﻿namespace WheelAuction.WebUI.Client.Components.Shared.Enumerations;
+
+public enum AlertMessageType
+{
+	Primary,
+	Secondary,
+	Success,
+	Info,
+	Warning,
+	Danger
+}
---
+++ src/WheelAuction.WebUI.Client/Resources/Components/Shared/AlertMessageResourceNames.cs
@@ -0,0 +1,6 @@
+﻿namespace WheelAuction.WebUI.Client.Resources.Components.Shared;
+
+internal static class AlertMessageResourceNames
+{
+	public const string ButtonAlertClose = "Button_Alert_Close";
+}
