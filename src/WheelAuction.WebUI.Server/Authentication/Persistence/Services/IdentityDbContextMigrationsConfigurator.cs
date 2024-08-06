25200c09da107f057b3d41acb99f3d82a82f7139
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:15:47 +0300

    feat(authentication): add service for configuring migrations

---
+++ src/WheelAuction.WebUI.Server/Authentication/Persistence/Services/IdentityDbContextMigrationsConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WheelAuction.Infrastructure.Persistence.Services.Abstractions;
+
+namespace WheelAuction.WebUI.Server.Authentication.Persistence.Services;
+
+internal class IdentityDbContextMigrationsConfigurator(IServiceScopeFactory serviceScopeFactory) : IMigrationsConfigurator
+{
+	private readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
+
+	public async Task ApplyAsync()
+	{
+		await using AsyncServiceScope scope = _serviceScopeFactory.CreateAsyncScope();
+		IdentityDbContext identityDbContext = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+		identityDbContext.Database.Migrate();
+	}
+}
