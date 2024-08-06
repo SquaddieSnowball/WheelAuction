ab4b3b3de1c75db667e3dfc1016a55a00a8f0975
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:36:27 +0300

    feat(domain): add services registration

---
+++ src/WheelAuction.Domain/Extensions/DependencyInjection.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WheelAuction.Domain.Extensions;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddDomain(this IServiceCollection services) => services;
+}
