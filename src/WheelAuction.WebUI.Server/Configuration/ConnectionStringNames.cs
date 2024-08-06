31801987a9090521e80892cc55919d78ce73965d
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:49:43 +0300

    feat(configuration): add a type containing connection string names

---
+++ src/WheelAuction.WebUI.Server/Configuration/ConnectionStringNames.cs
@@ -0,0 +1,7 @@
+﻿namespace WheelAuction.WebUI.Server.Configuration;
+
+internal static class ConnectionStringNames
+{
+	public const string ContainerDb = "ContainerDb";
+	public const string LocalDb = "LocalDb";
+}
