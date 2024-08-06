e0122a1c8175eaab9c6c3e57e99653b6303c9937
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:23:06 +0300

    feat(configuration): add options to configure authentication

---
+++ src/WheelAuction.WebUI.Server/Configuration/Options/AuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿using WheelAuction.WebUI.Server.Configuration.Options.InputModels;
+
+namespace WheelAuction.WebUI.Server.Configuration.Options;
+
+internal class AuthenticationOptions
+{
+	public const string ConfigurationSectionName = "Authentication";
+
+	public AuthenticationOptionsUserInputModel MainUser { get; set; } = default!;
+
+	public IEnumerable<AuthenticationOptionsUserInputModel>? AdditionalUsers { get; set; }
+}
---
+++ src/WheelAuction.WebUI.Server/Configuration/Options/InputModels/AuthenticationOptionsUserInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WheelAuction.WebUI.Server.Configuration.Options.InputModels;
+
+internal class AuthenticationOptionsUserInputModel
+{
+	public string UserName { get; set; } = default!;
+
+	public string Password { get; set; } = default!;
+
+	public string? RoleName { get; set; }
+}
