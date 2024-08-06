90dace4f5fcc0467561f7ce3f917c5e57dbe91f1
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:34:49 +0300

    feat(page): add registration page

---
+++ src/WheelAuction.WebUI.Server/Components/Account/InputModels/RegisterInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WheelAuction.WebUI.Server.Components.Account.InputModels;
+
+internal class RegisterInputModel
+{
+	public string UserName { get; set; } = default!;
+
+	public string Password { get; set; } = default!;
+
+	public string ConfirmPassword { get; set; } = default!;
+}
---
+++ src/WheelAuction.WebUI.Server/Resources/Components/Account/Pages/RegisterResourceNames.cs
@@ -0,0 +1,16 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Components.Account.Pages;
+
+internal static class RegisterResourceNames
+{
+	public const string PageTitle = "Page_Title";
+	public const string PageHeader = "Page_Header";
+
+	public const string LabelInputUserName = "Label_Input_UserName";
+	public const string LabelInputPassword = "Label_Input_Password";
+	public const string LabelInputConfirmPassword = "Label_Input_ConfirmPassword";
+	public const string ButtonFormRegisterSubmit = "Button_Form_Register_Submit";
+
+	public const string AlertMessageRegistrationErrorTitle = "AlertMessage_RegistrationError_Title";
+	public const string AlertMessageRegistrationErrorDescriptionCommon = "AlertMessage_RegistrationError_Description_Common";
+	public const string AlertMessageRegistrationErrorDescriptionUserExists = "AlertMessage_RegistrationError_Description_UserExists";
+}
