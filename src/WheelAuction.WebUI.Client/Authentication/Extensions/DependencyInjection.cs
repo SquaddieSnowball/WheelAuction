edb3d9fa023ea7d5f6d91f0b66201771bac10fb4
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:55:33 +0300

    feat(authentication): add services registration

---
+++ src/WheelAuction.WebUI.Client/Authentication/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using WheelAuction.WebUI.Client.Authentication.Services;
+
+namespace WheelAuction.WebUI.Client.Authentication.Extensions;
+
+internal static class DependencyInjection
+{
+	public static IServiceCollection AddAuthenticationServices(this IServiceCollection services)
+	{
+		services
+			.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>()
+			.AddCascadingAuthenticationState();
+
+		return services;
+	}
+}
