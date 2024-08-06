6dfa27208f10f78e09f902602601f2f7c6776163
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:33:51 +0300

    feat(validation): add validation to the input model of the login page

---
+++ src/WheelAuction.WebUI.Server/Resources/Validation/Validators/Components/Account/InputModels/LoginInputModelValidatorResourceNames.cs
@@ -0,0 +1,7 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Validation.Validators.Components.Account.InputModels;
+
+internal static class LoginInputModelValidatorResourceNames
+{
+	public const string ErrorMessageUserNameEmpty = "ErrorMessage_UserName_Empty";
+	public const string ErrorMessagePasswordEmpty = "ErrorMessage_Password_Empty";
+}
---
+++ src/WheelAuction.WebUI.Server/Validation/Validators/Components/Account/InputModels/LoginInputModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using WheelAuction.WebUI.Server.Components.Account.InputModels;
+using WheelAuction.WebUI.Server.Resources.Validation.Validators.Components.Account.InputModels;
+
+namespace WheelAuction.WebUI.Server.Validation.Validators.Components.Account.InputModels;
+
+internal class LoginInputModelValidator : AbstractValidator<LoginInputModel>
+{
+	public LoginInputModelValidator(IStringLocalizer<LoginInputModelValidator> stringLocalizer)
+	{
+		RuleFor(loginInputModel => loginInputModel.UserName)
+			.NotEmpty()
+			.WithMessage(stringLocalizer[LoginInputModelValidatorResourceNames.ErrorMessageUserNameEmpty]);
+
+		RuleFor(loginInputModel => loginInputModel.Password)
+			.NotEmpty()
+			.WithMessage(stringLocalizer[LoginInputModelValidatorResourceNames.ErrorMessagePasswordEmpty]);
+	}
+}
