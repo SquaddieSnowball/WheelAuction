f54a1a61d82540eedc761ca312b508f87dfab345
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:08:30 +0300

    feat(authentication): add entities for authentication

---
+++ src/WheelAuction.WebUI.Server/Authentication/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WheelAuction.WebUI.Server.Authentication.Entities;
+
+internal class Role : IdentityRole
+{
+	public static Role Create(string name) => new()
+	{
+		Name = name
+	};
+}
---
+++ src/WheelAuction.WebUI.Server/Authentication/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WheelAuction.WebUI.Server.Authentication.Entities;
+
+internal class User : IdentityUser
+{
+	public static User Create(string userName) => new()
+	{
+		UserName = userName
+	};
+}
