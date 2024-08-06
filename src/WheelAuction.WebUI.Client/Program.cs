678caa5f398c34ff1350124c510c7a967605116f
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 18:30:07 +0300

    feat(client): add registration of additional services at startup process

--- src/WheelAuction.WebUI.Client/Program.cs
+++ src/WheelAuction.WebUI.Client/Program.cs
@@ -1,6 +1,20 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WheelAuction.WebUI.Client.Authorization.Extensions;
+using WheelAuction.WebUI.Client.Services.Abstractions;
+using WheelAuction.WebUI.Client.Services;
+using WheelAuction.WebUI.Client.Authentication.Extensions;
 
 WebAssemblyHostBuilder hostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
+
+hostBuilder.Services
+	.AddLocalization();
+
+hostBuilder.Services
+	.AddScoped<IRedirectManager, RedirectManager>();
+
+hostBuilder.Services
+	.AddAuthenticationServices()
+	.AddConfiguredAuthorization();
 
 WebAssemblyHost host = hostBuilder.Build();
 
