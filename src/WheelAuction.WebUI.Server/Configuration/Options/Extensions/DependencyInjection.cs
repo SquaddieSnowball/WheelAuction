8c681a5d189c4e0c5d1a5e4f52946cc0bbb1810f
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:13:25 +0300

    feat(configuration): add services registration

---
+++ src/WheelAuction.WebUI.Server/Configuration/Options/Extensions/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿namespace WheelAuction.WebUI.Server.Configuration.Options.Extensions;
+
+internal static class DependencyInjection
+{
+	public static IServiceCollection AddConfiguredOptions(this IServiceCollection services)
+	{
+		services
+			.AddOptionsWithFluentValidation<AuthenticationOptions>(AuthenticationOptions.ConfigurationSectionName);
+
+		return services;
+	}
+
+	public static IServiceCollection AddOptionsWithFluentValidation<TOptions>(
+		this IServiceCollection services,
+		string configurationSectionName)
+		where TOptions : class
+	{
+		services
+			.AddOptions<TOptions>()
+			.BindConfiguration(configurationSectionName)
+			.ValidateFluentValidation();
+
+		return services;
+	}
+}
