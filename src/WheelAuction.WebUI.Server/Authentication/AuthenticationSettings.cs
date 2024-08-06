ec5f588902c6bca2d1bba5a3b7db04f951e6c0bd
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:26:37 +0300

    feat(authentication): add authentication settings

---
+++ src/WheelAuction.WebUI.Server/Authentication/AuthenticationSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace WheelAuction.WebUI.Server.Authentication;
+
+internal static class AuthenticationSettings
+{
+	public static readonly bool SignInRequireConfirmedAccount = false;
+	public static readonly bool SignInRequireConfirmedEmail = false;
+	public static readonly bool SignInRequireConfirmedPhoneNumber = false;
+
+	public static readonly bool LockoutAllowedForNewUsers = true;
+	public static readonly int LockoutTimeMinutes = 5;
+	public static readonly int LockoutMaxFailedAccessAttempts = 5;
+
+	public static readonly int UserNameMinLength = 3;
+	public static readonly int UserNameMaxLength = 30;
+	public static readonly string UserNameAllowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+	public static readonly int PasswordMinLength = 8;
+	public static readonly int PasswordRequiredUniqueCharsCount = 2;
+	public static readonly bool PasswordRequireLowercaseChar = false;
+	public static readonly bool PasswordRequireUppercaseChar = false;
+	public static readonly bool PasswordRequireDigit = false;
+	public static readonly bool PasswordRequireNonAlphanumericChar = false;
+}
