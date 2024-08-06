a0c973cee1c44cf0b1e8495c76791582363f20d8
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:50:51 +0300

    feat(server): add environment helper

---
+++ src/WheelAuction.WebUI.Server/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace WheelAuction.WebUI.Server.Helpers;
+
+internal static class EnvironmentHelper
+{
+	private const string IsContainerEnvironmentVariable = "DOTNET_RUNNING_IN_CONTAINER";
+
+	public static bool IsContainer => Environment.GetEnvironmentVariable(IsContainerEnvironmentVariable) is "true";
+}
