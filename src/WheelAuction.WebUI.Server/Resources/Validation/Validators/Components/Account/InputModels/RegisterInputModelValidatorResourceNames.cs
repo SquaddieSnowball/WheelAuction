328f46cbbe77e654388b7820fcc1901b3b53d2a4
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:36:29 +0300

    feat(validation): add validation to the input model of the registration page

---
+++ src/WheelAuction.WebUI.Server/Resources/Validation/Validators/Components/Account/InputModels/RegisterInputModelValidatorResourceNames.cs
@@ -0,0 +1,11 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Validation.Validators.Components.Account.InputModels;
+
+internal static class RegisterInputModelValidatorResourceNames
+{
+	public const string ErrorMessageUserNameEmpty = "ErrorMessage_UserName_Empty";
+	public const string ErrorMessageUserNameInvalid = "ErrorMessage_UserName_Invalid";
+	public const string ErrorMessagePasswordEmpty = "ErrorMessage_Password_Empty";
+	public const string ErrorMessagePasswordInvalid = "ErrorMessage_Password_Invalid";
+	public const string ErrorMessageConfirmPasswordEmpty = "ErrorMessage_ConfirmPassword_Empty";
+	public const string ErrorMessageConfirmPasswordMissmatch = "ErrorMessage_ConfirmPassword_Missmatch";
+}
---
+++ src/WheelAuction.WebUI.Server/Validation/Validators/Components/Account/InputModels/RegisterInputModelValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using WheelAuction.WebUI.Server.Authentication;
+using WheelAuction.WebUI.Server.Components.Account.InputModels;
+using WheelAuction.WebUI.Server.Resources.Validation.Validators.Components.Account.InputModels;
+using WheelAuction.WebUI.Server.Validation.Extensions;
+
+namespace WheelAuction.WebUI.Server.Validation.Validators.Components.Account.InputModels;
+
+internal class RegisterInputModelValidator : AbstractValidator<RegisterInputModel>
+{
+	public RegisterInputModelValidator(IStringLocalizer<RegisterInputModelValidator> stringLocalizer)
+	{
+		RuleFor(registerInputModel => registerInputModel.UserName)
+			.Cascade(CascadeMode.Stop)
+			.NotEmpty()
+			.WithMessage(stringLocalizer[RegisterInputModelValidatorResourceNames.ErrorMessageUserNameEmpty])
+			.UserName()
+			.WithMessage(stringLocalizer[
+				RegisterInputModelValidatorResourceNames.ErrorMessageUserNameInvalid,
+				AuthenticationSettings.UserNameMinLength,
+				AuthenticationSettings.UserNameMaxLength]);
+
+		RuleFor(registerInputModel => registerInputModel.Password)
+			.Cascade(CascadeMode.Stop)
+			.NotEmpty()
+			.WithMessage(stringLocalizer[RegisterInputModelValidatorResourceNames.ErrorMessagePasswordEmpty])
+			.Password()
+			.WithMessage(stringLocalizer[
+				RegisterInputModelValidatorResourceNames.ErrorMessagePasswordInvalid,
+				AuthenticationSettings.PasswordMinLength,
+				AuthenticationSettings.PasswordRequiredUniqueCharsCount]);
+
+		RuleFor(registerInputModel => registerInputModel.ConfirmPassword)
+			.Cascade(CascadeMode.Stop)
+			.NotEmpty()
+			.WithMessage(stringLocalizer[RegisterInputModelValidatorResourceNames.ErrorMessageConfirmPasswordEmpty])
+			.Equal(registerInputModel => registerInputModel.Password)
+			.WithMessage(stringLocalizer[RegisterInputModelValidatorResourceNames.ErrorMessageConfirmPasswordMissmatch]);
+	}
+}
