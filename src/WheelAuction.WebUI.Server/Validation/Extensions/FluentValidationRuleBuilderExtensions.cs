9b225ae920a1558ab6d1efd94fdf8a9eca65c7b7
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:13:51 +0300

    feat(validation): add a type containing extensions to the Fluent Validation rule builder

---
+++ src/WheelAuction.WebUI.Server/Validation/Extensions/FluentValidationRuleBuilderExtensions.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation;
+using WheelAuction.WebUI.Client.Authentication.Enumerations;
+using WheelAuction.WebUI.Server.Authentication;
+
+namespace WheelAuction.WebUI.Server.Validation.Extensions;
+
+internal static class FluentValidationRuleBuilderExtensions
+{
+	public static IRuleBuilderOptions<T, string?> UserName<T>(this IRuleBuilder<T, string?> ruleBuilder) =>
+		ruleBuilder.Must(str =>
+		{
+			if (str is null)
+				return true;
+
+			if (!IsValidUserNameLength(str))
+				return false;
+
+			if (!IsValidUserNameCharacters(str))
+				return false;
+
+			return true;
+		});
+
+	public static IRuleBuilderOptions<T, string?> Password<T>(this IRuleBuilder<T, string?> ruleBuilder) =>
+		ruleBuilder.Must(str =>
+		{
+			if (str is null)
+				return true;
+
+			if (!IsValidPasswordLength(str))
+				return false;
+
+			if (!IsValidPasswordCharacters(str))
+				return false;
+
+			return true;
+		});
+
+	public static IRuleBuilderOptions<T, string?> RoleName<T>(this IRuleBuilder<T, string?> ruleBuilder) =>
+		ruleBuilder.IsEnumName(typeof(RoleName));
+
+	private static bool IsValidUserNameLength(string userName) =>
+		(userName.Length >= AuthenticationSettings.UserNameMinLength) &&
+		(userName.Length <= AuthenticationSettings.UserNameMaxLength);
+
+	private static bool IsValidUserNameCharacters(string userName) =>
+		!userName.Any(c => !AuthenticationSettings.UserNameAllowedCharacters.Contains(c));
+
+	private static bool IsValidPasswordLength(string password) =>
+		password.Length >= AuthenticationSettings.PasswordMinLength;
+
+	private static bool IsValidPasswordCharacters(string password)
+	{
+		if (password.Distinct().Count() < AuthenticationSettings.PasswordRequiredUniqueCharsCount)
+			return false;
+
+		if (AuthenticationSettings.PasswordRequireNonAlphanumericChar && !password.Any(c => !char.IsLetterOrDigit(c)))
+			return false;
+
+		if (AuthenticationSettings.PasswordRequireLowercaseChar && !password.Any(char.IsLower))
+			return false;
+
+		if (AuthenticationSettings.PasswordRequireUppercaseChar && !password.Any(char.IsUpper))
+			return false;
+
+		if (AuthenticationSettings.PasswordRequireDigit && !password.Any(char.IsDigit))
+			return false;
+
+		return true;
+	}
+}
