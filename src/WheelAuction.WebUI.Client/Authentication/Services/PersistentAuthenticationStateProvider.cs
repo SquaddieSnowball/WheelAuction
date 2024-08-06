af5a5892a4be1b73138d2f96ce53910df10c05e0
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:54:22 +0300

    feat(authentication): add implementation for client side authentication state provider

---
+++ src/WheelAuction.WebUI.Client/Authentication/Services/PersistentAuthenticationStateProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using WheelAuction.WebUI.Client.Authentication.Entities;
+
+namespace WheelAuction.WebUI.Client.Authentication.Services;
+
+internal class PersistentAuthenticationStateProvider : AuthenticationStateProvider
+{
+	private static readonly Task<AuthenticationState> s_defaultAuthenticationStateTask =
+		Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+
+	private readonly Task<AuthenticationState> _authenticationStateTask = s_defaultAuthenticationStateTask;
+
+	public PersistentAuthenticationStateProvider(PersistentComponentState persistentComponentState)
+	{
+		AuthenticationStateInfo? authenticationStateInfo = GetAuthenticationStateInfo(persistentComponentState);
+
+		if (authenticationStateInfo is null)
+			return;
+
+		IEnumerable<Claim> claims = GenerateClaims(authenticationStateInfo);
+
+		_authenticationStateTask =
+			Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims))));
+	}
+
+	public override Task<AuthenticationState> GetAuthenticationStateAsync() => _authenticationStateTask;
+
+	private AuthenticationStateInfo? GetAuthenticationStateInfo(PersistentComponentState persistentComponentState)
+	{
+		persistentComponentState.TryTakeFromJson(
+			nameof(AuthenticationStateInfo),
+			out AuthenticationStateInfo? authenticationStateInfo);
+
+		return authenticationStateInfo;
+	}
+
+	private static IEnumerable<Claim> GenerateClaims(AuthenticationStateInfo authenticationStateInfo) =>
+		[
+			new Claim(ClaimTypes.Name, authenticationStateInfo.UserName),
+			new Claim(ClaimTypes.Role, authenticationStateInfo.RoleName)
+		];
+}
