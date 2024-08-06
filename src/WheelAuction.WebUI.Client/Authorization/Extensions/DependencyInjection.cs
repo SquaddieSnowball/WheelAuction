ea007a1e2cabb08e2547de823e8dd89b433f12d5
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 18:00:01 +0300

    feat(authorization): add services registration

---
+++ src/WheelAuction.WebUI.Client/Authorization/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using WheelAuction.WebUI.Client.Authorization.Policies.Extensions;
+
+namespace WheelAuction.WebUI.Client.Authorization.Extensions;
+
+internal static class DependencyInjection
+{
+	public static IServiceCollection AddConfiguredAuthorization(this IServiceCollection services)
+	{
+		services
+			.AddAuthorizationCore(authorizationOptions =>
+			{
+				authorizationOptions
+					.AddAllPolicies();
+			});
+
+		return services;
+	}
+}
