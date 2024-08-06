dbf6bae7c97989b3cd33f2e9d7261937fd1d2ca8
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:58:53 +0300

    feat(authorization): add a type containing extension methods for adding policies

---
+++ src/WheelAuction.WebUI.Client/Authorization/Policies/Extensions/AuthorizationOptionsExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using WheelAuction.WebUI.Client.Authentication.Enumerations;
+
+namespace WheelAuction.WebUI.Client.Authorization.Policies.Extensions;
+
+public static class AuthorizationOptionsExtensions
+{
+	public static AuthorizationOptions AddAllPolicies(this AuthorizationOptions authorizationOptions)
+	{
+		authorizationOptions.
+			AddRoleRequirementPolicies();
+
+		return authorizationOptions;
+	}
+
+	public static AuthorizationOptions AddRoleRequirementPolicies(this AuthorizationOptions authorizationOptions)
+	{
+		authorizationOptions.AddPolicy(PolicyNames.Administrator, authorizationPolicyBuilder =>
+		{
+			authorizationPolicyBuilder.RequireRole(
+				RoleName.Administrator.ToString());
+		});
+
+		authorizationOptions.AddPolicy(PolicyNames.User, authorizationPolicyBuilder =>
+		{
+			authorizationPolicyBuilder.RequireRole(
+				RoleName.Administrator.ToString(),
+				RoleName.User.ToString());
+		});
+
+		return authorizationOptions;
+	}
+}
