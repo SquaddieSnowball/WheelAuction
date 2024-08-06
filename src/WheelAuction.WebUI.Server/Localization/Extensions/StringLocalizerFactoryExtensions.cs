55ed7ecc5e278862c4c402d28335b45bc3241228
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:52:34 +0300

    feat(server): add a type containing extensions to the localizer factory

---
+++ src/WheelAuction.WebUI.Server/Localization/Extensions/StringLocalizerFactoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace WheelAuction.WebUI.Server.Localization.Extensions;
+
+internal static class StringLocalizerFactoryExtensions
+{
+	public static IStringLocalizer? CreateForGenericService(
+		this IStringLocalizerFactory stringLocalizerFactory,
+		Type serviceType)
+	{
+		if (!serviceType.IsGenericType)
+			return default;
+
+		string location = serviceType.Assembly.GetName().Name!;
+		string baseName = $"{serviceType.Namespace}.{serviceType.Name[..serviceType.Name.IndexOf('`')]}"[(location.Length + 1)..];
+
+		return stringLocalizerFactory.Create(baseName, location);
+	}
+}
