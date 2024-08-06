90522545ea19eb9341512c9c35e386735ef812ce
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 20:12:54 +0300

    feat(authentication): add DbContext configuration

---
+++ src/WheelAuction.WebUI.Server/Authentication/Persistence/DbContextSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace WheelAuction.WebUI.Server.Authentication.Persistence;
+
+internal static class DbContextSettings
+{
+	public const string DefaultSchema = "identity";
+}
---
+++ src/WheelAuction.WebUI.Server/Authentication/Persistence/IdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WheelAuction.WebUI.Server.Authentication.Entities;
+using WheelAuction.WebUI.Server.Shared;
+
+namespace WheelAuction.WebUI.Server.Authentication.Persistence;
+
+internal class IdentityDbContext(DbContextOptions<IdentityDbContext> dbContextOptions)
+	: IdentityDbContext<User, Role, string>(dbContextOptions)
+{
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.HasDefaultSchema(DbContextSettings.DefaultSchema);
+		modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+	}
+}
