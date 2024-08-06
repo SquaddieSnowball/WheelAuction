6da540c1dbc9eda243135289183e0485bb818ea3
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:19:04 +0300

    feat(server): add a type containing extensions to the endpoint route builder

---
+++ src/WheelAuction.WebUI.Server/Authentication/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WheelAuction.WebUI.Server.Authentication.Endpoints;
+using WheelAuction.WebUI.Server.Authentication.Entities;
+using WheelAuction.WebUI.Server.Logging.Authentication;
+
+namespace WheelAuction.WebUI.Server.Authentication.Extensions;
+
+internal static class EndpointRouteBuilderExtensions
+{
+	public static IEndpointConventionBuilder MapAdditionalAccountEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+	{
+		RouteGroupBuilder accountRouteGroupBuilder = endpointRouteBuilder.MapGroup(EndpointRouteGroups.Account);
+
+		accountRouteGroupBuilder
+			.MapLogoutEndpoint();
+
+		return accountRouteGroupBuilder;
+	}
+
+	private static IEndpointRouteBuilder MapLogoutEndpoint(this IEndpointRouteBuilder endpointRouteBuilder)
+	{
+		endpointRouteBuilder.MapPost(EndpointRoutes.Logout, async (
+			[FromServices] ILoggerFactory loggerFactory,
+			[FromServices] SignInManager<User> signInManager,
+			[FromForm] string userName,
+			[FromForm] string returnUrl) =>
+		{
+			await signInManager.SignOutAsync();
+
+			ILogger logger = loggerFactory.CreateLogger(
+				$"{typeof(EndpointRouteGroups).Namespace}." +
+				$"{nameof(EndpointRouteGroups.Account)}." +
+				nameof(EndpointRoutes.Logout));
+
+			LogAuthentication.UserLoggedOut(logger, userName);
+
+			return TypedResults.LocalRedirect($"~/{returnUrl}");
+		});
+
+		return endpointRouteBuilder;
+	}
+}
