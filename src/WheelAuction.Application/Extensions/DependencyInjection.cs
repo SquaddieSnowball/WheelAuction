a59bb386c141a8b6c4053a9450779f891aa7563c
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:37:37 +0300

    feat(application): add services registration

---
+++ src/WheelAuction.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WheelAuction.Application.Extensions;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddApplication(this IServiceCollection services) => services;
+}
