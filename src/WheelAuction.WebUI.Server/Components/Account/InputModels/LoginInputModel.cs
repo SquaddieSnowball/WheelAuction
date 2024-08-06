db657aa440e9fef08b00f811bc59829174c6d353
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:32:18 +0300

    feat(page): add login page

---
+++ src/WheelAuction.WebUI.Server/Components/Account/InputModels/LoginInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WheelAuction.WebUI.Server.Components.Account.InputModels;
+
+internal class LoginInputModel()
+{
+	public string UserName { get; set; } = default!;
+
+	public string Password { get; set; } = default!;
+
+	public bool RememberMe { get; set; }
+}
---
+++ src/WheelAuction.WebUI.Server/Resources/Components/Account/Pages/LoginResourceNames.cs
@@ -0,0 +1,19 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Components.Account.Pages;
+
+internal static class LoginResourceNames
+{
+	public const string PageTitle = "Page_Title";
+	public const string PageHeader = "Page_Header";
+
+	public const string LabelInputUserName = "Label_Input_UserName";
+	public const string LabelInputPassword = "Label_Input_Password";
+	public const string LabelInputRememberMe = "Label_Input_RememberMe";
+	public const string ButtonFormLoginSubmit = "Button_Form_Login_Submit";
+
+	public const string TextNoAccount = "Text_NoAccount";
+	public const string LinkRegister = "Link_Register";
+
+	public const string AlertMessageLoginErrorTitle = "AlertMessage_LoginError_Title";
+	public const string AlertMessageLoginErrorDescriptionCommon = "AlertMessage_LoginError_Description_Common";
+	public const string AlertMessageLoginErrorDescriptionLockedOut = "AlertMessage_LoginError_Description_LockedOut";
+}
