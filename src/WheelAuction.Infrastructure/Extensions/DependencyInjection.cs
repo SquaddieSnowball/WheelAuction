6b873de50ea8a2292e11916fa95f1c29e81bb158
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:46:21 +0300

    feat(infrastructure): add services registration

---
+++ src/WheelAuction.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using WheelAuction.Infrastructure.Persistence;
+using WheelAuction.Infrastructure.Persistence.Services;
+using WheelAuction.Infrastructure.Persistence.Services.Abstractions;
+
+namespace WheelAuction.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddInfrastructure(this IServiceCollection services, string applicationDbConnectionString)
+	{
+		services
+			.AddDbContext<ApplicationDbContext>(optionsBuilder =>
+			{
+				optionsBuilder.UseNpgsql(applicationDbConnectionString, npgsqlOptionsBuilder =>
+				{
+					npgsqlOptionsBuilder.MigrationsHistoryTable(
+						HistoryRepository.DefaultTableName,
+						DbContextSettings.DefaultSchema);
+				});
+			})
+			.AddSingleton<IMigrationsConfigurator, ApplicationDbContextMigrationsConfigurator>();
+
+		return services;
+	}
+}
