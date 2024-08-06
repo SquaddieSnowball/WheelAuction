381f24d33c9fd3c545365fcd42b14f1465939f34
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 18:08:31 +0300

    feat(client): add a service to perform redirects

---
+++ src/WheelAuction.WebUI.Client/Services/Abstractions/IRedirectManager.cs
@@ -0,0 +1,8 @@
+﻿namespace WheelAuction.WebUI.Client.Services.Abstractions;
+
+public interface IRedirectManager
+{
+	public void RedirectTo(string? uri, bool forceLoad = false);
+
+	public void RedirectTo(string? uri, IEnumerable<KeyValuePair<string, object?>> queryParameters, bool forceLoad = false);
+}
---
+++ src/WheelAuction.WebUI.Client/Services/RedirectManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using WheelAuction.WebUI.Client.Services.Abstractions;
+
+namespace WheelAuction.WebUI.Client.Services;
+
+public class RedirectManager(NavigationManager navigationManager) : IRedirectManager
+{
+	private readonly NavigationManager _navigationManager = navigationManager;
+
+	public void RedirectTo(
+		[StringSyntax(StringSyntaxAttribute.Uri)] string? uri,
+		bool forceLoad = false)
+	{
+		uri = NormalizeUri(uri);
+
+		_navigationManager.NavigateTo(uri, forceLoad);
+	}
+
+	public void RedirectTo(
+		[StringSyntax(StringSyntaxAttribute.Uri)] string? uri,
+		IEnumerable<KeyValuePair<string, object?>> queryParameters,
+		bool forceLoad = false)
+	{
+		uri = NormalizeUri(uri);
+		uri = _navigationManager.GetUriWithQueryParameters(uri, queryParameters.ToDictionary());
+
+		_navigationManager.NavigateTo(uri, forceLoad);
+	}
+
+	private string NormalizeUri(string? uri)
+	{
+		uri ??= string.Empty;
+
+		if (!Uri.IsWellFormedUriString(uri, UriKind.Relative))
+			uri = _navigationManager.ToBaseRelativePath(uri);
+
+		return uri;
+	}
+}
