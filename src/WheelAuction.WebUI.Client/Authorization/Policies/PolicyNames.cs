df8f6c2230c5b423be787f0ac15042b0cc37cc3c
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 17:57:12 +0300

    feat(authorization): add a type containing policy names

---
+++ src/WheelAuction.WebUI.Client/Authorization/Policies/PolicyNames.cs
@@ -0,0 +1,7 @@
+﻿namespace WheelAuction.WebUI.Client.Authorization.Policies;
+
+public static class PolicyNames
+{
+	public const string Administrator = "Administrator";
+	public const string User = "User";
+}
