300c3b631d19bc90bc2e23f4e10d1b9bea118c7e
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-06 21:27:56 +0300

    feat(components): add main header

---
+++ src/WheelAuction.WebUI.Server/Resources/Components/Shared/MainHeaderResourceNames.cs
@@ -0,0 +1,11 @@
+﻿namespace WheelAuction.WebUI.Server.Resources.Components.Shared;
+
+internal static class MainHeaderResourceNames
+{
+	public const string NavbarMainBrand = "Navbar_Main_Brand";
+	public const string NavbarMainTogglerLabel = "Navbar_Main_Toggler_Label";
+	public const string NavbarMainLinkDashboard = "Navbar_Main_Link_Dashboard";
+	public const string NavbarMainLinkLogin = "Navbar_Main_Link_Login";
+	public const string NavbarMainLinkManageAccount = "Navbar_Main_Link_ManageAccount";
+	public const string NavbarMainLinkLogout = "Navbar_Main_Link_Logout";
+}
