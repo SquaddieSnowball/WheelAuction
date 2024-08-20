296dd8279084a73d2b8345b3af4af8b3d811161b
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-20 16:42:14 +0300

    feat(application): add registration of validation services

--- src/WheelAuction.Application/Extensions/DependencyInjection.cs
+++ src/WheelAuction.Application/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using WheelAuction.Application.Shared;
 
 namespace WheelAuction.Application.Extensions;
@@ -12,6 +13,11 @@ public static class DependencyInjection
 			{
 				configuration.RegisterServicesFromAssembly(AssemblyReference.Assembly);
 			});
+
+		services
+			.AddValidatorsFromAssembly(
+				AssemblyReference.Assembly,
+				includeInternalTypes: true);
 
 		return services;
 	}
