ec76f4909aaa341ff88c3863a6ebb74f882a1192
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2024-08-20 16:47:52 +0300

    feat(client): add registration of additional services

--- src/WheelAuction.WebUI.Client/Program.cs
+++ src/WheelAuction.WebUI.Client/Program.cs
@@ -1,10 +1,21 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WheelAuction.WebUI.Client.Authorization.Extensions;
-using WheelAuction.WebUI.Client.Services.Abstractions;
-using WheelAuction.WebUI.Client.Services;
+using WheelAuction.Application.Extensions;
 using WheelAuction.WebUI.Client.Authentication.Extensions;
+using WheelAuction.WebUI.Client.Authorization.Extensions;
+using WheelAuction.WebUI.Client.Services;
+using WheelAuction.WebUI.Client.Services.Abstractions;
+using WheelAuction.WebUI.Client.Shared;
 
 WebAssemblyHostBuilder hostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
+
+hostBuilder.Services
+	.AddApplicationServices();
+
+hostBuilder.Services
+	.AddValidatorsFromAssembly(
+		AssemblyReference.Assembly,
+		includeInternalTypes: true);
 
 hostBuilder.Services
 	.AddLocalization();
